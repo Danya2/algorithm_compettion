ca7499e15ba55665c9bc2aaf30970a1012bb7a2c
Shengke Li <danya@pku.edu.cn>
2019-07-24 00:19:39 +0800

    Add files via upload

--- chapter_4/4_9.cpp
+++ chapter_4/4_9.cpp
@@ -1,1 +1,20 @@
-sdasd
+#include<stdio.h>
+int sum(int *a, int *b);
+int main()
+{
+  int a[] = {1,2,3,4,5,6,7};
+  int value;
+  value= sum(a+1,a+5);
+  printf("sum value:%ld \n",value);
+  return 0;
+}
+
+int sum(int *a , int *b)
+{
+  int n=*b-*a;
+  int ans;
+  for(int i=0; i<n;i++)
+       ans+=a[i];
+  return ans;
+}
+
